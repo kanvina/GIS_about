ceb0eddc449182270d31cfe9171a9a7e44e25dd0
wangyandi <you@example.com>
2019-10-28 10:37:41 +0800

    井网构造实验

--- 井网构造实验.py
+++ 井网构造实验.py
@@ -16,64 +16,60 @@ from matplotlib import pyplot as plt
 from matplotlib.pyplot import MultipleLocator
 
 
-def init_five_cell(cell_len,center_location,x_zoom,y_zoom,theta,gamma,Delta_x,Delta_y):
-    '''
-    初始化五点式井组单元
-    :param cell_len: 井组单元边长
-    :param center_location: 井组中心点位置[x,y]
-    :return:
-    '''
+class well_grid():
 
-    x_cell_len=cell_len*x_zoom
-    y_cell_len=cell_len*y_zoom
+    def __init__(self):
 
-
-    point_LD_location=[center_location[0]-(cell_len/2)+Delta_x,center_location[1]-(cell_len/2)+Delta_y]
-    point_LT_location=[y_cell_len*np.cos((theta-gamma)*np.pi/180)+point_LD_location[0],y_cell_len*np.sin((theta-gamma)*np.pi/180)+point_LD_location[1]]
+        self.cell_len = 100
+        self.center_location = [50, 50]
+        self.x_zoom = 1
+        self.y_zoom = 1
+        self.theta = 60
+        self.gamma = 15
+        self.Delta_x = 50
+        self.Delta_y = 50
 
 
-    angle_a= (np.arctan( (np.cos(theta*np.pi/180)+(y_cell_len/x_cell_len) )/np.sin(theta*np.pi/180)))*180/np.pi
-    angle_b=180-theta-angle_a
-    angle_c=angle_a-90+theta-gamma
-    diagonal_line_len = y_cell_len*np.sin(angle_a*np.pi/180)+x_cell_len*np.sin(angle_b*np.pi/180)
 
-    point_RT_location=[ diagonal_line_len*np.cos(angle_c*np.pi/180)+point_LD_location[0],diagonal_line_len*np.sin(angle_c*np.pi/180)+point_LD_location[1]  ]
-    point_RD_location=[ x_cell_len*np.cos(gamma*np.pi/180)+point_LD_location[0],point_LD_location[1]-x_cell_len*np.sin(gamma*np.pi/180) ]
+    def init_five_cell(self):
 
-    print(point_LD_location)
-    print(point_LT_location)
-    print(point_RT_location)
+        x_cell_len=self.cell_len*self.x_zoom
+        y_cell_len=self.cell_len*self.y_zoom
 
-    print((point_LD_location[0]-point_LT_location[0])**2+(point_LD_location[1]-point_LT_location[1])**2)
-    print((point_RT_location[0] - point_LT_location[0]) ** 2 + (point_RT_location[1] - point_LT_location[1]) ** 2)
+        angle_a= (np.arctan( (np.cos(self.theta*np.pi/180)+(y_cell_len/x_cell_len) )/np.sin(self.theta*np.pi/180)))*180/np.pi
+        angle_b=180-self.theta-angle_a
+        angle_c=angle_a-90+self.theta-self.gamma
+        diagonal_line_len = y_cell_len*np.sin(angle_a*np.pi/180)+x_cell_len*np.sin(angle_b*np.pi/180)
 
-    plt.scatter(point_LD_location[0], point_LD_location[1], marker='x', color='red', s=40, label='First')
+
+        point_LD_location=[self.center_location[0]-(self.cell_len/2)+self.Delta_x,self.center_location[1]-(self.cell_len/2)+self.Delta_y]
+        point_LT_location=[y_cell_len*np.cos((self.theta-self.gamma)*np.pi/180)+point_LD_location[0],y_cell_len*np.sin((self.theta-self.gamma)*np.pi/180)+point_LD_location[1]]
+
+        point_RT_location=[ diagonal_line_len*np.cos(angle_c*np.pi/180)+point_LD_location[0],diagonal_line_len*np.sin(angle_c*np.pi/180)+point_LD_location[1]  ]
+        point_RD_location=[ x_cell_len*np.cos(self.gamma*np.pi/180)+point_LD_location[0],point_LD_location[1]-x_cell_len*np.sin(self.gamma*np.pi/180) ]
+        point_center_location=[ 0.5*diagonal_line_len*np.cos((angle_a+self.theta-self.gamma-90)*np.pi/180)+point_LD_location[0],0.5*diagonal_line_len*np.sin((angle_a+self.theta-self.gamma-90)*np.pi/180)+point_LD_location[1] ]
+        return [point_LD_location,point_LT_location,point_RT_location,point_RD_location,point_center_location]
+
+
+
+def draw_scatter(points):
+    point_LD_location=points[0]
+    point_LT_location=points[1]
+    point_RT_location=points[2]
+    point_RD_location=points[3]
+    point_center_location=points[4]
+    plt.scatter(point_LD_location[0], point_LD_location[1], marker='o', color='red', s=40, label='First')
     plt.scatter(point_LT_location[0], point_LT_location[1], marker='o', color='red', s=40, label='First')
-    plt.scatter(point_RT_location[0], point_RT_location[1], marker='x', color='red', s=40, label='First')
+    plt.scatter(point_RT_location[0], point_RT_location[1], marker='o', color='red', s=40, label='First')
     plt.scatter(point_RD_location[0], point_RD_location[1], marker='o', color='red', s=40, label='First')
-
-    x_major_locator = MultipleLocator(10)
-    # 把x轴的刻度间隔设置为1，并存在变量里
-    y_major_locator = MultipleLocator(10)
-    # 把y轴的刻度间隔设置为10，并存在变量里
-    ax = plt.gca()
-    # ax为两条坐标轴的实例
-    ax.xaxis.set_major_locator(x_major_locator)
-    # 把x轴的主刻度设置为1的倍数
-    ax.yaxis.set_major_locator(y_major_locator)
+    plt.scatter(point_center_location[0], point_center_location[1], marker='x', color='red', s=40, label='First')
 
     plt.show()
 
-
 if __name__=="__main__":
 
-    cell_len=100
-    center_location=[50,50]
-    x_zoom=1
-    y_zoom=1
-    theta=60
-    gamma=15
-    Delta_x=50
-    Delta_y=50
+    grid=well_grid()
+    points=grid.init_five_cell()
+    draw_scatter(points)
 
-    init_five_cell(cell_len, center_location, x_zoom, y_zoom, theta, gamma, Delta_x, Delta_y)
+
