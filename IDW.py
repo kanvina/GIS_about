f78703c35b6e7276503e6d6c086bb42163d27c23
wangyandi <1069704056@qq.com>
2020-01-28 11:42:02 +0800

    WYD 2020 01 28 11

--- IDW.py
+++ IDW.py
@@ -28,14 +28,14 @@ def get_grid_size(range,cell_len):
     return row_size,column_size
 #获取中心点坐标
 def get_cell_center(row_i,column_i,cell_len,data_range):
-    x_center=(column_i*cell_len)+(cell_len/2)
+    x_center=data_range[0]+(column_i*cell_len)+(cell_len/2)
     y_center=data_range[3]-(row_i*cell_len)-(cell_len/2)
     center_loction=[x_center,y_center]
     return center_loction
 #
 
 
-def get_z(data_arr,center_loction,p):
+def get_z(data_arr,center_loction,p,n):
     distance_list =[]
     for point_x_y_z in data_arr:
         point_xi=point_x_y_z[0]
@@ -44,46 +44,61 @@ def get_z(data_arr,center_loction,p):
         distance_list.append(distance)
     wi_num=0
     wi_list= []
-    for distance_point in distance_list:
+    distance_list=np.array(distance_list)
+    distance_sort_list=np.sort(distance_list)[0:12]
+    distance_idx_list=np.argsort(distance_list)[0:12]
+
+
+    for distance_point in distance_sort_list:
         wi_point=distance_point**-p
         wi_num=wi_num+wi_point
         wi_list.append(wi_point)
     z_i_sum=0
     for i in range(len(wi_list)):
+        idx=distance_idx_list[i]
         wi_list[i]=wi_list[i]/wi_num
-        z_i=data_arr[i,2]*wi_list[i]
+        z_i=data_arr[idx,n]*wi_list[i]
         z_i_sum=z_i_sum+z_i
 
     return z_i_sum
 
-def run_IDW(data_arr, row_size, column_size,cell_len,data_range,p):
+def run_IDW(data_arr, row_size, column_size,cell_len,data_range,p,n):
     data_IDW=np.zeros((row_size,column_size))
     for index in range(row_size*column_size):
         row_i=int(np.floor(index/column_size))
         column_i=index%column_size
         center_loction=get_cell_center(row_i,column_i,cell_len,data_range)
-        z=get_z(data_arr,center_loction,p)
+        z=get_z(data_arr,center_loction,p,n)
         data_IDW[row_i,column_i]=z
     return data_IDW
 
-def main_IDW(path_point_xls,cell_len,p):
+def main_IDW(path_point_xls,cell_len,p,n):
+    '''
+
+    :param path_point_xls:
+    :param cell_len:
+    :param p:
+    :param n: 待插值列数
+    :return:
+    '''
     data_arr = get_array_excel(path_point_xls)
     data_range = get_range(data_arr)
     row_size, column_size = get_grid_size(data_range, cell_len)
-    data_IDW = run_IDW(data_arr, row_size, column_size, cell_len, data_range, p)
+    data_IDW = run_IDW(data_arr, row_size, column_size, cell_len, data_range, p,n)
     return data_IDW,data_range
 
 if __name__ =="__main__":
 
     path_point_xls='data/data_points.xls'
     cell_len = 10
-    p=2
+    p=2#。幂指数
+    n=2#待插值列数
 
-    data_IDW,data_range=main_IDW(path_point_xls, cell_len, p)
+    data_IDW,data_range=main_IDW(path_point_xls, cell_len, p,n)
     print(data_IDW)
     print(data_range)
 
-    pd.DataFrame(data_IDW).to_excel('data_out/data_IDW.xls',index=0)
+    pd.DataFrame(data_IDW).to_csv('data/data_IDW.csv',index=0)
 
     plt.imshow(data_IDW)
     plt.show()
